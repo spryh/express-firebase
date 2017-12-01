5d64a730e6f1021b25b0ee20e7a740554eaffa41
Paul Scarpa <spryh@Bootstrap.home>
2017-11-30 17:50:36 -0800

    Adding Express and routing

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const express = require('express')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const helloWorld = express()
+
+helloWorld.get('/helloworld', (request, response) =>{
+    response.send("Hello from Firebase!");    
+})
+
+exports.helloWorld = functions.https.onRequest((helloWorld) => {
+});
